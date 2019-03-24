7517212dda89a7e93daf6772594876e031643574
Matt Hughes <mattw.hughes@gmail.com>
2019-03-24 19:36:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   valid_move?(board, index)
-  if valid_move? == move
+  if valid_move?(board, index) == move
 end
 
 end
